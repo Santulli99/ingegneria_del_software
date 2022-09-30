89cdb1f50e87bce031c34e8c675bb541abefddd3
Andrea <a.santulli3@studenti.unisa.it>
2022-09-30 15:54:29 +0200

    Update

--- src/main/java/class_test/Test.java
+++ src/main/java/class_test/Test.java
@@ -1,7 +1,10 @@
 package class_test;
 
 public class Test {
+    int num=1;
     public static void main (String[] args){
-        System.out.println("test1");
+        System.out.println("test1 ");
+
     }
+
 }
