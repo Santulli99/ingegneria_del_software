d58ab4eb495dbd83723892469ff89bb301ff5f7c
gerryos95 <espositogerardo@live.it>
2022-09-30 15:22:00 +0200

    start2

--- src/main/java/class_test/Test.java
+++ src/main/java/class_test/Test.java
@@ -1,7 +1,7 @@
 package class_test;
 
 public class Test {
-    public static void main (String args[]){
-        System.out.println("test");
+    public static void main (String[] args){
+        System.out.println("test1");
     }
 }
